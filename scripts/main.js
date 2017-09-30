2807202222fe871580ca2a5901d5d8661b78af4a
GMarguiller <gael.marguiller@supinternet.fr>
2017-09-30 15:32:40 +0200

    Add new task

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,15 @@
                 {title: "Arseler la caf", isDone: false}
             ]
         },
+        methods: {
+            addTask: function() {
+                this.tasks.push({
+                    isDone: false,
+                    title: this.newTask
+                })
+                this.newTask = ''
+            }
+        },
         computed: {
             remaining() {
                 return this.tasks.filter(task => !task.isDone).length
